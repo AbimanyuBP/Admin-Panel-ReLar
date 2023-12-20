be2f93c872a2bb89756c69213a3af8e3689f0822
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2023-12-20 14:05:05 +0700

    Added new model and controller

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('img');
+            $table->string('status');
+            $table->integer('age');
         });
     }
 
