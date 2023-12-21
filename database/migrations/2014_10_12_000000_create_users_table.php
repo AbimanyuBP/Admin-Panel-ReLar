c3e3f150a486c3aab364e9ca8791db3b53eb213d
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2023-12-21 10:33:22 +0700

    Added default value for columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,9 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->string('img');
-            $table->string('status');
-            $table->integer('age');
+            $table->string('img')->default("https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500");
+            $table->string('status')->default("passive");
+            $table->integer('age')->default(21);
         });
     }
 
